6a36232c76815827e6a836a2bb197e362d95b1b9
Dima Matroskin <dima.matroskin062@gmail.com>
2021-03-09 18:39:17 +0200

    added new methods

--- src/API.js
+++ src/API.js
@@ -1,12 +1,12 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'https://jsonplaceholder.typicode.com/photos',
+  baseURL: 'https://jsonplaceholder.typicode.com/',
 });
 
 const PhotosAPI = {
   getPhotos: async (offset = 1) => {
-    const { data } = await instance.get('', {
+    const { data } = await instance.get('photos/', {
       params: {
         _page: offset,
         _limit: 6,
@@ -15,7 +15,17 @@ const PhotosAPI = {
     return data;
   },
   getPhoto: async (id) => {
-    const { data } = await instance.get(`/${id}`);
+    const { data } = await instance.get(`photos/${id}?_expand=album`);
+    return data;
+  },
+  getUser: async (id) => {
+    const { data } = await instance.get(`albums/${id}?_expand=user`);
+    return data.user;
+  },
+  getAlbumPhotos: async (id) => {
+    const { data } = await instance.get(
+      `photos?albumId=${id}&_offset=0&_limit=6`
+    );
     return data;
   },
 };
