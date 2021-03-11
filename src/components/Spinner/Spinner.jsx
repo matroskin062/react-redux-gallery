98f1c6b90f8ebfe35745e7ccdbcbbecbb9c0138f
Dima Matroskin <dima.matroskin062@gmail.com>
2021-03-12 00:52:54 +0200

    svg inserted directly to spinner component

--- src/components/Spinner/Spinner.jsx
+++ src/components/Spinner/Spinner.jsx
@@ -1,11 +1,38 @@
 import React from 'react';
 
-import spinner from '../../assets/spinner.svg';
-
 const Spinner = () => {
   return (
     <div style={{ textAlign: 'center', width: '100%' }}>
-      <img src={spinner} alt='' />
+      <svg
+        xmlns='http://www.w3.org/2000/svg'
+        style={{ margin: 'auto', background: '0 0' }}
+        width='200'
+        height='200'
+        display='block'
+        preserveAspectRatio='xMidYMid'
+        viewBox='0 0 100 100'
+      >
+        <defs />
+        <circle
+          cx='50'
+          cy='50'
+          r='33'
+          fill='none'
+          stroke='#483d8b'
+          stroke-dasharray='51.83627878423159 51.83627878423159'
+          stroke-linecap='round'
+          stroke-width='5'
+        >
+          <animateTransform
+            attributeName='transform'
+            dur='1.3513513513513513s'
+            keyTimes='0;1'
+            repeatCount='indefinite'
+            type='rotate'
+            values='0 50 50;360 50 50'
+          />
+        </circle>
+      </svg>
     </div>
   );
 };
